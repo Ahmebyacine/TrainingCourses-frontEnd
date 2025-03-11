a4cd79d91243c284fe4d5d2af4e8e62a72e082c8
ahmedbldy <benaoun-abdelmouhaimen@univ-eloued.dz>
2025-03-11 14:11:47 +0100

    fix baseUrl

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/TrainingCourses-frontEnd/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss()
